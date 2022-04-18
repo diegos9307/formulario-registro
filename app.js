c0fc90e4a25b473fecb21ae0efdc67e3d7f88a8a
diegos9307 <diegos9307@gmail.com>
2022-04-18 15:27:46 -0500

    implementando solución, pendiente que funcione bootstrap

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const hbs = require("express-handlebars");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost:27017/mongo-1",
+  { useNewUrlParser: true }
+);
+
+const UsuarioSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String },
+  password: { type: String },
+});
+const Usuario = mongoose.model("Usuario", UsuarioSchema);
+
+// handlebars setup
+app.set("views", path.join(__dirname, "views"));
+
+app.set(
+  ".hbs",
+  hbs.engine({
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.get("/", async (req, res) => {
+  let usuarios = await Usuario.find();
+  res.render("index", { usuarios });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/", async (req, res) => {
+  const usuario = new Usuario(req.body);
+  await usuario.save();
+  let usuarios = await Usuario.find();
+  res.render("index", { usuarios });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT} ...`)
+);
